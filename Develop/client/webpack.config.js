130d4c14959f21c9d26fc96ce1e080fb896ab44e
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-08-18 21:25:25 -0400

    initial commit

---
+++ Develop/client/webpack.config.js
@@ -0,0 +1,77 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const path = require('path');
+const { InjectManifest } = require('workbox-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: {
+      main: './src/js/index.js',
+      install: './src/js/install.js',
+      editor: "./src/js/editor.js",
+      header: "./src/js/header.js"
+    },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'JATE',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'JATE',
+        short_name: 'JATE',
+        description: 'Just Another Text Editor',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ], 
+      }),
+      new InjectManifest({ 
+        swSrc: './src-sw.js', //source from
+        swDest: 'src-sw.js', //destination 
+      }),
+      new MiniCssExtractPlugin(),
+    ],
+
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
+         test: /\.m?js$/,
+         exclude: /(node_modules|bower_components)/,
+         use: {
+          loader: 'babel-loader',
+          options: {
+          presents: ['@babel/present-env'],
+          plugins:['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          },
+         }, 
+        },
+      ],
+    },
+  };
+};
