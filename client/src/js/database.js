2c191d5aea2d3121890710c96cf367e549dd348c
Mike Gonzalez <Mike.Gon1217@gmail.com>
2022-08-26 19:02:41 -0400

    Pushing to Heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,37 +1,38 @@
-import { openDB } from 'idb';
+import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB("jate", 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains("jate")) {
+        console.log("jate database already exists");
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
+      console.log("jate db created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => {
+// method that accepts some content and adds it to the database
+export const putDb = async (id, content) => {
+  console.error("putDb not implemented");
   const db = await openDB("jate", 1);
-  const trans = db.transaction("jate", "readwrite");
-  const store = trans.objectStore("jate");
-  const request = store.put({ id: 1, value: content });
+  const tx = db.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: id, jate: content });
   const result = await request;
-  console.log("Data saved to db", result.value);
-
-
+  console.log("Data saved to database", result);
 };
 
-// TODO: Add logic for a method that gets all the content from the database
+// method that gets all the content from the database
 export const getDb = async () => {
-  const db = await openDB('jate', 1);
-  const trans = db.transaction('jate', 'readonly');
-  const store = trans.objectStore('jate');
-  const request = store.get(1);
+  console.error("getDb not implemented");
+  const db = await openDB("jate", 1);
+  const trans = db.transaction("jate", "readonly");
+  const store = trans.objectStore("jate");
+  const request = store.getAll();
   const result = await request;
-  console.log('result.value', result.value);
+  console.log("result.value", result);
   return result.value;
-}
+};
+
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,71 +1,75 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js',
-      editor: './src/js/editor.js',
-      header: './src/js/header.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      editor: "./src/js/editor.js",
+      header: "./src/js/header.js",
     },
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'JATE',
+        template: "./index.html",
+        title: "JATE",
       }),
-      new InjectManifest({ 
-        swSrc: './src-sw.js', //source from
-        swDest: 'src-sw.js', //send to
+      new MiniCssExtractPlugin(),
+      new InjectManifest({
+        swSrc: "./src-sw.js", //source from
+        swDest: "src-sw.js", //destination
       }),
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: 'JATE',
-        short_name: 'JATE',
-        description: 'Just Another Text Editor',
-        background_color: '#225ca3',
-        theme_color: '#225ca3',
-        start_url: './',
-        publicPath: './',
+        name: "JATE",
+        short_name: "JATE",
+        description: "Just Another Text Editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "./",
+        publicPath: "./",
         icons: [
           {
-            src: path.resolve('src/images/logo.png'),
+            src: path.resolve("src/images/logo.png"),
             sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
+            destination: path.join("assets", "icons"),
           },
-        ], 
+        ],
       }),
-      new MiniCssExtractPlugin(),
     ],
 
     module: {
       rules: [
         {
           test: /\.css$/i,
-          use: [MiniCssExtractPlugin.loader, 'css-loader'],
+          use: [MiniCssExtractPlugin.loader, "css-loader"],
         },
         {
-         test: /\.m?js$/,
-         exclude: /(node_modules|bower_components)/,
-         use: {
-          loader: 'babel-loader',
-          options: {
-          presets: ['@babel/preset-env'],
-          plugins:['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
           },
-         }, 
         },
       ],
     },
